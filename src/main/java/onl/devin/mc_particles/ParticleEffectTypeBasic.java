986a7501b3d3cf5800e0bf7b4e104fdf4edf07ae
devinengr <79027895+devinengr@users.noreply.github.com>
2024-07-04 13:42:17 -0400

    add basic particle type

---
+++ src/main/java/onl/devin/mc_particles/ParticleEffectTypeBasic.java
@@ -0,0 +1,23 @@
+package onl.devin.mc_particles;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+public class ParticleEffectTypeBasic implements ParticleEffectType {
+
+    private Vector relativeParticleLocation;
+
+    @Override
+    public void updateRelativeParticleLocation(ParticleEffect effect) {
+        relativeParticleLocation = effect.getTrajectory().getLocation().toVector();
+    }
+
+    @Override
+    public void spawnParticle(ParticleEffect effect) {
+        Player player = effect.getTrajectory().getPlayer();
+        Location loc = relativeParticleLocation.toLocation(player.getWorld());
+        player.getWorld().spawnParticle(effect.getParticle(), loc, effect.getParticleCount());
+    }
+
+}
